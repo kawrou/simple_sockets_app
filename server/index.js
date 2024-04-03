a3d875c668d55994b2043367efa99570bb7a3848
kawrou <121672581+kawrou@users.noreply.github.com>
2024-04-03 18:33:09 +0100

    Basic server code for creating rooms, and emitting messages

--- server/index.js
+++ server/index.js
@@ -15,11 +15,29 @@ const server = http.createServer(app);
 const io = new Server(server, {
 	//Configure our cors
 	cors: {
+		//Our react app is running on 3000
 		origin: "http://localhost:3000",
 		methods: ["GET", "POST"],
 	},
 });
 
+//Listen to events on our new instance of Server class
+io.on("connection", (socket) => {
+	console.log(`User Connected: ${socket.id}`);
+
+	//Listens for the event "join_room"
+	socket.on("join_room", (roomId) => {
+		socket.join(roomId);
+	});
+
+	//Listens for the event "send_message"
+	socket.on("send_message", (data) => {
+		console.log(data);
+		socket.to(data.room).emit("receive_message", data);
+	});
+});
+
+//Our server is on 3001
 server.listen(3001, () => {
 	console.log("SERVER IS RUNNING");
 });
